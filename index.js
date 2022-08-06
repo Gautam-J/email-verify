7e0a02ec29a1fb0e7b25501fac5aead5d88e47b5
Gautam J <gautam.jayapal@gmail.com>
2022-08-06 11:05:30 +0530

    add database support

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const connectToDB = require("./database/db");
 
 const app = express();
+
+connectToDB();
 
 app.use(morgan("dev"));
 app.use(express.json());
@@ -11,6 +14,11 @@ app.use(cors());
 // default empty route to check API
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+// check authorization of email
+app.post("/verify", (req, res) => {
+  res.send("Verify");
 });
 
 // either PORT env variable or 4000
